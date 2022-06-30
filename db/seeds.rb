0fd81c5d72a000d0f73752b485bc54d021b4b261
keoshayoung <keoshayoung@email.com>
2022-06-30 14:16:34 -0400

    updates seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-product = Product.new(name: "Red Electric Bike", price: "99", image_url: "https://media.wired.com/photos/6116c71586407d6dcacb9272/master/pass/Gear-Cannondale-Adventure-Neo-3.jpg", description: "Brand New Electric Bike")
+product = Product.new(name: "Cannondale Adventure Neo 3 Electric Bike", price: "3000", image_url: "https://media.wired.com/photos/6116c71586407d6dcacb9272/master/pass/Gear-Cannondale-Adventure-Neo-3.jpg", description: "Clean design. Reliable components from Shimano and Bosch. Excellent built-in lights. Great shocks and suspension seat post. Built-in rear rack and fenders. Easy to ride thanks to mid-drive motor.")
 product.save
 
-product = Product.new(name: "Yellow Electric Bike", price: "49", image_url: "https://media.wired.com/photos/61afb905d184762c75e00411/master/w_2560%2Cc_limit/Gear-Jackbrabbit-Bike-Yellow-top.jpg", description: "Brand New Electric Bike")
+product = Product.new(name: "Jackrabbit Electric Bike", price: "1000", image_url: "https://media.wired.com/photos/61afb905d184762c75e00411/master/w_2560%2Cc_limit/Gear-Jackbrabbit-Bike-Yellow-top.jpg", description: "So adorable. Incredibly light and portable. No chain or gearing to squeak or fix. Fun to ride, if you're short. Tons of accessories. So adorable.")
 product.save
 
-product = Product.new(name: "Black All-Terrain Electric Bike", price: "149", image_url: "https://cdn.shopify.com/s/files/1/0255/9528/1483/products/cruiser_ad750f00-b51f-4899-a8d8-50af97e45262_1800x1800.jpg?v=1645780958", description: "Brand New Electric Bike")
+product = Product.new(name: "Himiway All-Terrain Electric Bike", price: "1599", image_url: "https://cdn.shopify.com/s/files/1/0255/9528/1483/products/cruiser_ad750f00-b51f-4899-a8d8-50af97e45262_1800x1800.jpg?v=1645780958", description: "Designing an elegant, sophisticated and versatile electric bike has always been the ultimate goal of Himiway. The low-step design provides convenience and exceptional comfort while retaining all the excellent performance featured in Cruiser. The 26 inches fat tires offer decent skid resistance and anti-vibration ability. Ride with convenience; Ride with elegance; Ride with us!")
 product.save
 
-product = Product.new(name: "Foldable Electric Bike", price: "49", image_url: "https://m.media-amazon.com/images/I/71FXwiDM2YS._AC_SL1500_.jpg", description: "Brand New Electric Bike")
+product = Product.new(name: "Ancheer Electric Bike", price: "719", image_url: "https://m.media-amazon.com/images/I/71FXwiDM2YS._AC_SL1500_.jpg", description: "The electric bicycle adopts 100 percent lightweight aluminum alloy collapsible frame for quick folding and easy storage. You can have it with minimal storage space and easily store into any vehicle. (The pedal can not be folded.)")
 product.save
