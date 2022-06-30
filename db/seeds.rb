79ba195386955963b910d78c2a68ffb0794fe2b5
keoshayoung <keoshayoung@email.com>
2022-06-30 12:54:14 -0400

    created products in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "Red Electric Bike", price: "99", image_url: "https://media.wired.com/photos/6116c71586407d6dcacb9272/master/pass/Gear-Cannondale-Adventure-Neo-3.jpg", description: "Brand New Electric Bike")
+product.save
+
+product = Product.new(name: "Yellow Electric Bike", price: "49", image_url: "https://media.wired.com/photos/61afb905d184762c75e00411/master/w_2560%2Cc_limit/Gear-Jackbrabbit-Bike-Yellow-top.jpg", description: "Brand New Electric Bike")
+product.save
+
+product = Product.new(name: "Black All-Terrain Electric Bike", price: "149", image_url: "https://cdn.shopify.com/s/files/1/0255/9528/1483/products/cruiser_ad750f00-b51f-4899-a8d8-50af97e45262_1800x1800.jpg?v=1645780958", description: "Brand New Electric Bike")
+product.save
+
+product = Product.new(name: "Foldable Electric Bike", price: "49", image_url: "https://m.media-amazon.com/images/I/71FXwiDM2YS._AC_SL1500_.jpg", description: "Brand New Electric Bike")
+product.save
