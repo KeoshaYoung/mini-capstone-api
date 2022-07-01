49b37baca9dd5e295e8a04b2c881bd629771c1ce
keoshayoung <keoshayoung@email.com>
2022-07-01 16:15:20 -0400

    changes 'price' to integer in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_163405) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "price"
+    t.integer "price"
     t.string "image_url"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
