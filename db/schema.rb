7dfeba8b252a35729e81db02ffb64948f586ef92
keoshayoung <keoshayoung@email.com>
2022-07-11 13:56:18 -0400

    removes image_url from products

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_163405) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_175437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
-    t.string "image_url"
+    t.decimal "price", precision: 9, scale: 2
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
