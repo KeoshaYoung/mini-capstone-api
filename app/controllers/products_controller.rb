5cc9c2901fbacf9b073e56222ffb140e6825b842
keoshayoung <keoshayoung@email.com>
2022-07-18 13:28:48 -0400

    creates cartedproducts model and removes product_id from orders model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,11 @@ class ProductsController < ApplicationController
   before_action :aunthenticate_admin, except: [:index, :show]
   
   def index
-    @products = Product.all
+    if params [:category]
+      category = Category.find_by(name: params [:category])
+    @products = category.products
     render :index
+    end
   end
 
   def create
